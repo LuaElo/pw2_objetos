a639e70fc8dcee0ccc37aedaf5745af02102a6cc
LuaraEloahhhhh <luararodriguesmagalhaes25@gmail.com>
2023-03-07 08:56:19 -0300

    Criacao de Classe Anime

---
+++ src/Anime.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+public class Anime {
+
+    public int id;
+    public String nome;
+    public LocalDate datalancamento;
+    public String classificacaoIndicativa;
+    public String genero;
+    public String autor;
+    public int quantidadedeEpisodios;
+    public int temporadas;
+}
---
+++ src/AnimeTeste.java
@@ -0,0 +1,16 @@
+public class AnimeTeste {
+
+    public static void main(String[] args) {
+        //Classe nomeObjeto = new Classe();
+        Anime meuAnime = new Anime();
+        meuAnime.nome = "Super11";
+        meuAnime.quantidadedeEpisodios = 32;
+        meuAnime.temporadas = 1;
+
+        Anime meuAnimePreferido = new Anime();
+        meuAnimePreferido.nome = "Naruto";
+        meuAnimePreferido.quantidadedeEpisodios = 327;
+        meuAnimePreferido.temporadas = 5;
+
+    }
+}
